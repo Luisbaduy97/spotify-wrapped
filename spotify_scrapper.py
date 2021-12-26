7ad0c766ba1bf7f2eb06255d568915178a1326d5
Luis Navarrete <lnavarrete@kwantec.com>
2021-12-26 11:22:33 -0600

    Add df with song data

--- spotify_scrapper.py
+++ spotify_scrapper.py
@@ -4,19 +4,39 @@ from spotipy.oauth2 import SpotifyOAuth
 import warnings
 from datetime import datetime
 import datetime
+import pandas as pd
 warnings.filterwarnings('ignore') #avoid deprecation warning for access token
 
 load_dotenv()
 
 today = datetime.datetime.now()
 yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-print(int(today.timestamp())*1000)
-print(int(yesterday.timestamp())*1000)
 
 scope = "user-read-recently-played"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
+song_name = []
+artist_name = []
+song_duration = []
+played_at = []
+spotify_id = []
+
 results = sp.current_user_recently_played()
-for i in results['items']:
-    print(i['track']['name'])
+for song in results['items']:
+    song_name.append(song['track']['name'])
+    artist_name.append(song['track']['album']['artists'][0]['name'])
+    song_duration.append(song['track']['duration_ms'])
+    spotify_id.append(song['track']['id'])
+    played_at.append(song['played_at'])
+
+data_collection = {
+    "song_name": song_name,
+    "artist_name": artist_name,
+    "song_duration_ms": song_duration,
+    "spotify_id": spotify_id,
+    "played_at": played_at
+}
+
+data = pd.DataFrame(data=data_collection, columns=data_collection.keys())
+print(data['spotify_id'].value_counts())
